28bfd6f4adcb06c6aba66acf1c5c11f7c387ec44
xilefcul <avivchro@gmail.com>
2019-11-23 18:09:28 +0100

    java class for chat client

--- app/src/main/java/com/example/clientapp/MainActivity.kt
+++ app/src/main/java/com/example/clientapp/MainActivity.kt
@@ -36,6 +36,9 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+
+
+
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         return super.onCreateOptionsMenu(menu)
     }
--- app/src/main/java/com/example/clientapp/ui/ChatActivity.kt
+++ app/src/main/java/com/example/clientapp/ui/ChatActivity.kt
@@ -14,11 +14,7 @@ class ChatActivity : AppCompatActivity() {
         setContentView(R.layout.activity_chat)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
     }
 
 }
