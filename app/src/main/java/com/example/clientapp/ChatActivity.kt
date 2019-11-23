369198721f5dc3d5bdf7fce5e9a1b801ead7b6ee
xilefcul <avivchro@gmail.com>
2019-11-23 14:20:37 +0100

    added functionality to buttonon start page

---
+++ app/src/main/java/com/example/clientapp/ChatActivity.kt
@@ -0,0 +1,24 @@
+package com.example.clientapp
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_chat.*
+
+
+
+class ChatActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chat)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+}
--- app/src/main/java/com/example/clientapp/MainActivity.kt
+++ app/src/main/java/com/example/clientapp/MainActivity.kt
@@ -1,15 +1,39 @@
 package com.example.clientapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Button
+import com.example.clientapp.ui.ChatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var diaryButton = findViewById<Button>(R.id.button_diary)
+
+        diaryButton.setOnClickListener {
+            val intent = Intent(this@MainActivity, DiaryActivity::class.java)
+            startActivity(intent)
+        }
+
+        var historyButton = findViewById<Button>(R.id.button_history)
+        historyButton.setOnClickListener {
+            val intent = Intent(this@MainActivity, HistoryActivity::class.java)
+            startActivity(intent)
+        }
+
+        var chatButton = findViewById<Button>(R.id.button_chat)
+        chatButton.setOnClickListener {
+            val intent = Intent(this@MainActivity, ChatActivity::class.java)
+            startActivity(intent)
+        }
+
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -19,6 +43,8 @@ class MainActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return super.onOptionsItemSelected(item)
     }
+
+
 
 
 
