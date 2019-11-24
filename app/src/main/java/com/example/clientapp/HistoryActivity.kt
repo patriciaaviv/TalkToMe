04c492f7cb5f27382f08c4842f4d62ef4baec162
xilefcul <avivchro@gmail.com>
2019-11-24 08:53:43 +0100

    added chat functionality

--- app/src/main/java/com/example/clientapp/HistoryActivity.kt
+++ app/src/main/java/com/example/clientapp/HistoryActivity.kt
@@ -11,12 +11,7 @@ class HistoryActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_history)
-        setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
 
--- app/src/main/java/com/example/clientapp/MainActivity.kt
+++ app/src/main/java/com/example/clientapp/MainActivity.kt
@@ -37,18 +37,4 @@ class MainActivity : AppCompatActivity() {
 
 
 
-
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        return super.onCreateOptionsMenu(menu)
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return super.onOptionsItemSelected(item)
-    }
-
-
-
-
-
-
 }
